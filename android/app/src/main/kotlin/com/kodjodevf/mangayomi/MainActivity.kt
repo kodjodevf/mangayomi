fe5c69bdfb4c17930f85d367cdb9ec5f787a3c0b
Vedant Suralkar <vedants8879@gmail.com>
2024-10-13 19:02:33 +0530

    new changes

--- android/app/src/main/kotlin/com/kodjodevf/mangayomi/MainActivity.kt
+++ android/app/src/main/kotlin/com/kodjodevf/mangayomi/MainActivity.kt
@@ -1,3 +1,58 @@
+import android.os.StrictMode // Add this import at the top
+import android.os.Build
+
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable StrictMode for debug builds to detect potential crashes
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+
+        // Rest of your initialization code...
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.kodjodevf.mangayomi.libtorrentserver",
+            StandardMethodCodec.INSTANCE
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "start" -> {
+                    val config = call.argument<String>("config")
+                    try {
+                        val port = Libtorrentserver.start(config)
+                        result.success(port)
+                    } catch (e: Exception) {
+                        result.error("ERROR", e.message, null)
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
+
 package com.kodjodevf.mangayomi
 
 import androidx.annotation.NonNull
