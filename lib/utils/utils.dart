e3c8dc989618da4aea75d52a9fac285542c7da52
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-11-25 10:49:36 +0100

    +

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -8,7 +8,8 @@ Source? getSource(String lang, String name) {
     return sourcesList.lastWhere(
       (element) =>
           element.name!.toLowerCase() == name.toLowerCase() &&
-          element.lang == lang,
+          element.lang == lang &&
+          element.sourceCode != null,
       orElse: () => throw ("Error when getting source"),
     );
   } catch (_) {
