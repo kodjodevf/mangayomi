7bc3061485a6d17a466fd2711e8e30b37d92637d
Schnitzel5 <tangphilip3@gmail.com>
2025-01-24 00:25:11 +0100

    +

--- lib/modules/more/settings/appearance/appearance_screen.dart
+++ lib/modules/more/settings/appearance/appearance_screen.dart
@@ -299,6 +299,63 @@ class AppearanceScreen extends ConsumerWidget {
                           fontSize: 11, color: context.secondaryColor),
                     ),
                   ),
+                  ListTile(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text(
+                                l10n.reorder_navigation,
+                              ),
+                              content: SizedBox(
+                                  width: context.width(0.8),
+                                  child: ReorderableListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: 7,
+                                    itemBuilder: (context, index) {
+                                      final locale = AppLocalizations
+                                          .supportedLocales[index];
+                                      return RadioListTile(
+                                        dense: true,
+                                        contentPadding: const EdgeInsets.all(0),
+                                        value: locale,
+                                        groupValue: l10nLocale,
+                                        onChanged: (value) {
+                                          ref
+                                              .read(l10nLocaleStateProvider
+                                                  .notifier)
+                                              .setLocale(locale);
+                                          Navigator.pop(context);
+                                        },
+                                        title: Text(completeLanguageName(
+                                            locale.toLanguageTag())),
+                                      );
+                                    },
+                                    onReorder: (oldIndex, newIndex) {
+                                    },
+                                  )),
+                              actions: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TextButton(
+                                        onPressed: () async {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text(
+                                          l10n.cancel,
+                                          style: TextStyle(
+                                              color: context.primaryColor),
+                                        )),
+                                  ],
+                                )
+                              ],
+                            );
+                          });
+                    },
+                    title: Text(l10n.reorder_navigation),
+                  ),
                   SwitchListTile(
                       value: hideAnime,
                       title: Text(context.l10n.hide_anime),
