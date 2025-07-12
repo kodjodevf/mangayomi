3313e6e53aff730317be518f3f3ba2419677f977
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2025-07-12 20:26:31 +0200

    Don't focus searchbar when migrating
    The searchbar already has input.

--- lib/modules/manga/detail/widgets/migrate_screen.dart
+++ lib/modules/manga/detail/widgets/migrate_screen.dart
@@ -100,6 +100,7 @@ class _MigrationScreenScreenState extends ConsumerState<MigrationScreen> {
               });
             },
             controller: _textEditingController,
+            autofocus: false,
           ),
         ],
       ),
