5af59bcbaf3db47ced0299c7281f2441e7ee2e55
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-02-27 14:37:36 +0100

    fix #663

--- lib/modules/history/history_screen.dart
+++ lib/modules/history/history_screen.dart
@@ -338,22 +338,11 @@ class _HistoryTabState extends ConsumerState<HistoryTab>
     int? deleteId,
   ) async {
     await manga.chapters.load();
-    final chapters = manga.chapters;
     isar.writeTxnSync(() {
       isar.historys.deleteSync(deleteId!);
-      for (var chapter in chapters) {
-        isar.chapters.deleteSync(chapter.id!);
-        ref
-            .read(synchingProvider(syncId: 1).notifier)
-            .addChangedPart(ActionType.removeChapter, chapter.id, "{}", false);
-      }
-      isar.mangas.deleteSync(manga.id!);
       ref
           .read(synchingProvider(syncId: 1).notifier)
           .addChangedPart(ActionType.removeHistory, deleteId, "{}", false);
-      ref
-          .read(synchingProvider(syncId: 1).notifier)
-          .addChangedPart(ActionType.removeItem, manga.id, "{}", false);
     });
     if (context.mounted) {
       Navigator.pop(context);
