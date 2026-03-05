ad3fd085ea3bfeec6d05a1bf2674150e824953eb
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-03-05 16:37:15 +0100

    refactor: remove flutter_inappwebview_linux plugin and update flutter_inappwebview dependency to use git source

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -7,7 +7,6 @@
 #include "generated_plugin_registrant.h"
 
 #include <desktop_webview_window/desktop_webview_window_plugin.h>
-#include <flutter_inappwebview_linux/flutter_inappwebview_linux_plugin.h>
 #include <flutter_qjs/flutter_qjs_plugin.h>
 #include <gtk/gtk_plugin.h>
 #include <isar_community_flutter_libs/isar_flutter_libs_plugin.h>
@@ -24,9 +23,6 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) desktop_webview_window_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopWebviewWindowPlugin");
   desktop_webview_window_plugin_register_with_registrar(desktop_webview_window_registrar);
-  g_autoptr(FlPluginRegistrar) flutter_inappwebview_linux_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterInappwebviewLinuxPlugin");
-  flutter_inappwebview_linux_plugin_register_with_registrar(flutter_inappwebview_linux_registrar);
   g_autoptr(FlPluginRegistrar) flutter_qjs_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterQjsPlugin");
   flutter_qjs_plugin_register_with_registrar(flutter_qjs_registrar);
