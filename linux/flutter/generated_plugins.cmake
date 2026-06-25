ba0826e72048c02bee25990fe89e9ae65d699a0d
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-06-25 10:47:01 +0100

    chore: update dependencies and configure platform-specific plugin registration

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -19,6 +19,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
   flutter_discord_rpc_fork
+  jni
   rust_lib_mangayomi
 )
 
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -25,6 +25,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
   flutter_discord_rpc_fork
+  jni
   rust_lib_mangayomi
 )
 
