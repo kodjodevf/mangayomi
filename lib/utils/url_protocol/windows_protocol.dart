fc5e724f542df52719bc08d5db9759201adf2451
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-08-13 14:11:12 +0100

    chore: update Gradle and plugin versions, refactor Windows protocol handler, and update dependencies

--- lib/utils/url_protocol/windows_protocol.dart
+++ lib/utils/url_protocol/windows_protocol.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
-import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
-import 'package:win32/win32.dart';
+import 'package:win32_registry/win32_registry.dart';
 
 import './protocol.dart';
 
-const _hive = HKEY_CURRENT_USER;
+final _hive = CURRENT_USER;
 
 class WindowsProtocolHandler extends ProtocolHandler {
   @override
@@ -15,48 +14,25 @@ class WindowsProtocolHandler extends ProtocolHandler {
 
     final prefix = _regPrefix(scheme);
     final capitalized = scheme[0].toUpperCase() + scheme.substring(1);
-    final args = getArguments(arguments).map((a) => _sanitize(a));
     final cmd =
-        '${executable ?? Platform.resolvedExecutable} ${args.join(' ')}';
+        '${executable ?? Platform.resolvedExecutable} ${getArguments(arguments).map(_sanitize).join(' ')}';
 
-    _regCreateStringKey(_hive, prefix, '', 'URL:$capitalized');
-    _regCreateStringKey(_hive, prefix, 'URL Protocol', '');
-    _regCreateStringKey(_hive, '$prefix\\shell\\open\\command', '', cmd);
+    final rootKey = _hive.create(prefix);
+    rootKey.setValue('', RegistryValue.string('URL:$capitalized'));
+    rootKey.setValue('URL Protocol', RegistryValue.string(''));
+    rootKey
+        .create(r'shell\open\command')
+        .setValue('', RegistryValue.string(cmd));
   }
 
   @override
   void unregister(String scheme) {
     if (defaultTargetPlatform != TargetPlatform.windows) return;
 
-    final txtKey = TEXT(_regPrefix(scheme));
-    try {
-      RegDeleteTree(HKEY_CURRENT_USER, txtKey);
-    } finally {
-      free(txtKey);
-    }
+    _hive.removeSubkey(_regPrefix(scheme));
   }
 
-  String _regPrefix(String scheme) => 'SOFTWARE\\Classes\\$scheme';
-
-  int _regCreateStringKey(int hKey, String key, String valueName, String data) {
-    final txtKey = TEXT(key);
-    final txtValue = TEXT(valueName);
-    final txtData = TEXT(data);
-    try {
-      return RegSetKeyValue(
-        hKey,
-        txtKey,
-        txtValue,
-        REG_SZ,
-        txtData,
-        txtData.length * 2 + 2,
-      );
-    } finally {
-      free(txtKey);
-      free(txtValue);
-      free(txtData);
-    }
-  }
+  String _regPrefix(String scheme) => r'SOFTWARE\Classes\$scheme';
 
   String _sanitize(String value) {
     value = value.replaceAll(r'%s', '%1').replaceAll(r'"', '\\"');
