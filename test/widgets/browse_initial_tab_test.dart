441c854417843df17716d1da5fc478d1f011ed35
Mehakdeep Singh <118588258+mrandhawa14@users.noreply.github.com>
2026-08-22 12:15:44 -0700

    test: drop an import the merge left unused

--- test/widgets/browse_initial_tab_test.dart
+++ test/widgets/browse_initial_tab_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mangayomi/models/manga.dart';
